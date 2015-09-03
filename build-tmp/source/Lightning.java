138680ca7c500bd096abe2c1ed71f233ad872c56
unknown <LATAM1@697-334-Sauron.sf.k12.internal>
2015-09-03 11:12:06 -0700

    Lighning added

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -1,16 +1,32 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
 int startX=300;
 int startY=150;
 int endX=300;
 int endY=150;
 
-void setup()
+public void setup()
 {
   size(700,300);
 
   background(49,77,65);
 }
 
-void draw()
+public void draw()
 {
  strokeWeight(2);
  background(49,77,65);
@@ -47,3 +63,12 @@ ellipse(187,95,10,7);
 ellipse(210,95,10,7);
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
